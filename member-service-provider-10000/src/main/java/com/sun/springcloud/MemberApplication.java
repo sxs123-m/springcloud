a1a7e691219d4b7e92e9ecdc5240431b3544c0cc
Admin <13664664+qyxinhua_0@user.noreply.gitee.com>
2024-03-22 12:52:10 +0800

    3.22

---
+++ member-service-provider-10000/src/main/java/com/sun/springcloud/MemberApplication.java
@@ -0,0 +1,18 @@
+package com.sun.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @Author sun
+ * @Create 2024/3/22 12:49
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class MemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+}
